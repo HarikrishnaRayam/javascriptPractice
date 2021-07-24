be6f459bed943e318de2b2969699bb129845863d
Rayam Harikrishna <rayamharikrishna@gmail.com>
2021-07-24 15:02:19 +0530

    Update and rename Basics_composite.js to Given a number N, print 'yes' if it is composite else print 'no'.js

--- Given a number N, print 'yes' if it is composite else print 'no'.js
+++ Given a number N, print 'yes' if it is composite else print 'no'.js
@@ -1,3 +1,11 @@
+// Given a number N, print 'yes' if it is composite else print 'no'.
+// Sample Testcase :
+// INPUT
+// 123
+// OUTPUT
+// yes
+
+
 // Getting input via STDIN
 const readline = require("readline");
 
